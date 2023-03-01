5b1a34da2d4474055cbc4f78d57a3567710d4c94
Andrea Mazzon <mazzon@math.lmu.de>
2023-03-01 15:10:40 +0100

    Modification of the test to include the solution(s) to the exercise at page 38 of the script

--- montecarlovariancereduction/controlvariates/cliquetOptionTest.py
+++ montecarlovariancereduction/controlvariates/cliquetOptionTest.py
@@ -22,6 +22,10 @@ from generateBSReturns import GenerateBSReturns
 from generateBSReturnsWithArrays import GenerateBSReturnsWithArrays
 
 # processParameters
+from montecarlovariancereduction.controlvariates.fasterControlVariatesCliquetBS import FasterControlVariatesCliquetBS
+from montecarlovariancereduction.controlvariates.fasterControlVariatesCliquetBSWithArrays import \
+    FasterControlVariatesCliquetBSWithArrays
+
 r = 0.2
 sigma = 0.5
 
@@ -57,7 +61,15 @@ cliquetOptionWithArrays = CliquetOptionWithArrays(numberOfSimulations, maturity,
 cliquetOptionWithControlVariates = ControlVariatesCliquetBS(numberOfSimulations, maturity, numberOfTimeIntervals,
                              localFloor, localCap, globalFloor, globalCap, sigma, r)
 
+# ..with the faster implementation of control variates..
+fasterCliquetOptionWithControlVariates = \
+    FasterControlVariatesCliquetBS(numberOfSimulations, maturity, numberOfTimeIntervals,
+                                   localFloor, localCap, globalFloor, globalCap, sigma, r)
 
+# ..and with faster implementation of control variates, using arrays
+fasterCliquetOptionWithControlVariatesWithArrays = \
+    FasterControlVariatesCliquetBSWithArrays(numberOfSimulations, maturity, numberOfTimeIntervals,
+                                   localFloor, localCap, globalFloor, globalCap, sigma, r)
 
 numberOfTests = 30
 
@@ -65,11 +77,15 @@ pricesStandard = []
 pricesAV = []
 pricesCV = []
 pricesStandardWithArrays = []
+pricesFasterCV = []
+pricesFasterCVWithArrays = []
 
 timesStandard = []
 timesAV = []
 timesCV = []
 timesStandardWithArrays = []
+timesFasterCV = []
+timesFasterCVWithArrays = []
 
 for k in range(numberOfTests):
     # first we do it via standard Monte-Carlo
@@ -90,10 +106,10 @@ for k in range(numberOfTests):
 
     # then with control variates
     start = time.time()
-    returnsRealizationsAV = generator.generateReturnsAntitheticVariables()
-    priceAV = cliquetOption.getDiscountedPriceOfTheOption(returnsRealizationsAV, r)
+    returnsRealizationsCV = generator.generateReturnsAntitheticVariables()
+    priceCV = cliquetOptionWithControlVariates.getPriceViaControlVariates()
     end = time.time()
-    pricesCV.append(priceAV)
+    pricesCV.append(priceCV)
     timesCV.append(end - start)
 
     # ..and then with control variates using arrays
@@ -103,6 +119,20 @@ for k in range(numberOfTests):
     end = time.time()
     pricesStandardWithArrays.append(priceStandardWithArrays)
     timesStandardWithArrays.append(end - start)
+
+    # ..with the faster control variates
+    start = time.time()
+    priceFasterCV = fasterCliquetOptionWithControlVariates.getPriceViaControlVariates()
+    end = time.time()
+    pricesFasterCV.append(priceFasterCV)
+    timesFasterCV.append(end - start)
+
+    # ..and finally with the faster control variates using arrays
+    start = time.time()
+    priceFasterCVWithArrays = fasterCliquetOptionWithControlVariatesWithArrays.getPriceViaControlVariates()
+    end = time.time()
+    pricesFasterCVWithArrays.append(priceFasterCVWithArrays)
+    timesFasterCVWithArrays.append(end - start)
 
 
 print()
@@ -118,6 +148,12 @@ print()
 print("The variance of the prices using standard Monte-Carlo with arrays is ", np.var(pricesStandardWithArrays))
 
 print()
+print("The variance of the prices using faster ocntrol variates is ", np.var(pricesFasterCV))
+
+print()
+print("The variance of the prices using the faster Control variates with arrays is ", np.var(pricesFasterCVWithArrays))
+
+print()
 print()
 print("The average elapsed time using standard Monte-Carlo is ", np.mean(timesStandard))
 
@@ -130,3 +166,8 @@ print("The average elapsed time using Control variates is ", np.mean(timesCV))
 print()
 print("The average elapsed time using standard Monte-Carlo with arrays is ", np.mean(timesStandardWithArrays))
 
+print()
+print("The average elapsed time using the faster Control variates is ", np.mean(timesFasterCV))
+
+print()
+print("The average elapsed time using the faster Control variates with arrays is ", np.mean(timesFasterCVWithArrays))
